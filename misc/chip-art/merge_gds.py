a6162a2bbe2bd7e0eb82d8dbbbac542698ff2f46
lena <lena.conde-araujo@csedge.org>
2026-05-13 00:03:56 -0400

    pls

--- misc/chip-art/merge_gds.py
+++ misc/chip-art/merge_gds.py
@@ -2,7 +2,7 @@ import gdstk
 
 # Load both GDS files
 chip_art_lib = gdstk.read_gds("chip_art.gds")
-my_lib = gdstk.read_gds("tt_um_rcyaon.gds")
+my_lib = gdstk.read_gds("chip_without_art.gds")
 
 # Get the top cell from chip_art
 chip_art_cell = chip_art_lib.top_level()[0]
@@ -12,7 +12,7 @@ my_lib.add(chip_art_cell)
 
 # Reference it inside your top cell at position (x, y)
 my_top = my_lib.top_level()[0]
-my_top.add(gdstk.Reference(chip_art_cell, origin=(200, 200)))
+my_top.add(gdstk.Reference(chip_art_cell, origin=(30, 130)))
 
 # Save
-my_lib.write_gds("my_chip_with_art.gds")
+my_lib.write_gds("tt_um_rcyaon.gds")
